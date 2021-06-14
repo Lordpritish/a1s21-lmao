3035e43d9012b3ae4fbba3f4538b6d762f77adb7
pritish911 <NfdgxDx9LhUnn3V>
2021-06-14 20:35:40 +0530

    added code for computeBaconPath

--- src/main/java/ca/utoronto/utm/mcs/computeBaconPath.java
+++ src/main/java/ca/utoronto/utm/mcs/computeBaconPath.java
@@ -1,74 +1,79 @@
 package ca.utoronto.utm.mcs;
 
 import com.sun.net.httpserver.HttpExchange;
-import org.neo4j.driver.Driver;
-
-import java.io.IOException;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
+import org.neo4j.driver.internal.InternalPath;
+import org.neo4j.driver.types.Node;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-
-public class computeBaconNumber {
-
+public class computeBaconPath {
     private final Driver neo4jDriver;
     private byte[] result;
     private Map response;
     private final String BaconID = "nm0000102";
-    public computeBaconNumber(Driver driver) {
+    public computeBaconPath(Driver driver) {
         this.neo4jDriver = driver;
     }
 
     public void run(HttpExchange r)  throws IOException {
         try {
+            //get Request variables
             String body = Utils.convert(r.getRequestBody());
             JSONObject deserialized = new JSONObject(body);
+            JSONObject responseJSON = new JSONObject();
             OutputStream os = r.getResponseBody();
-            JSONObject responseJSON;
 
-            System.out.println("getRelationship handler get:");
-            System.out.println(deserialized);
             //If actorID is not given return 400 as BAD REQUEST
             if (!deserialized.has("actorID")){
                 r.sendResponseHeaders(400, -1);
             }
-            //actorID is given, then test for existence
+            //actorID is given, then to test existence
             else {
                 String actorID = deserialized.getString("actorID");
+                actorIDEXIST check=  new actorIDEXIST(this.neo4jDriver);
 
                 //Test if Kevin Bacon is in the database
-                actorIDEXIST check=  new actorIDEXIST(this.neo4jDriver);
                 Boolean bacon_NOT_Exist = check.run(r,this.BaconID);
 
                 //Test if actorID input is in the database
                 Boolean actor_NOT_Exist = check.run(r,actorID);
+
                 //neither Kevin Bacon or actor is not existed in the database
                 //responded 400 as NO ACTOR EXIST
                 if ((bacon_NOT_Exist) || (actor_NOT_Exist)){
                     r.sendResponseHeaders(404,-1);
                 }
-                //normal case to compute a baconNumber&baconPath
+                //both actors existed, try to find the baconPath
                 else {
-
-                    if (!actorID.equals(this.BaconID)) {
-                        //interaction with database
+                    if (!actorID.equals(BaconID)) {
+                        //interaction with database to calculate baconPath
                         try ( Session session = neo4jDriver.session() )
                         {
-                            response = Neo4jDAO.computeBaconNumber(session,actorID,this.BaconID);
+                            response = Neo4jDAO.computeBaconPath(session,actorID,this.BaconID);
                         }
-                        //result for server-client interaction
-                        responseJSON = new JSONObject(response);
+                        try {
+                            //add baconNumber response
+//                            responseJSON.put("baconPath", response.get("baconNumber"));
 
+//                            //add baconPath in a list<JSONObject> form
+                            responseJSON.put("baconPath", createBaconPath(response));
+                        }
+                        //actorID found but path not found in the database and 404 return as NO PATH FOUND
+                        catch (NullPointerException e) {
+                            r.sendResponseHeaders(404, -1);
+                        }
                     }
                     //actorID given is the same as actorID for Kevin Bacon
                     else {
-                        responseJSON = new JSONObject(response);
-                        responseJSON.put("baconNumber", "0");
+                        responseJSON.put("baconPath", "[]");
                     }
                     //valid actorID passed in and valid result responded by database
                     if (responseJSON.length() != 0) {
@@ -77,15 +82,11 @@ public class computeBaconNumber {
                         //write to a byte[] for OutputStream
                         os.write(result);
                     }
-                    //both actors found but no path in the database and 404 return as NO PATH FOUND
-                    else {
-                        r.sendResponseHeaders(404, -1);
-                    }
                 }
-                os.close();
             }
+            os.close();
         }
-        //if deserilized failed, (ex: JSONObeject Null Value)
+
         catch(JSONException e) {
             r.sendResponseHeaders(400, -1);
         }
@@ -93,6 +94,30 @@ public class computeBaconNumber {
         catch(Exception e) {
             r.sendResponseHeaders(500, -1);
         }
-
     }
+
+
+    private List<String> createBaconPath(Map response) throws JSONException {
+        System.out.println("createBaconPath is running:");
+        //change format of data from: Path ->Iterable<Node> ->List<JSONObject>
+        InternalPath path = (InternalPath) response.get("baconPath");
+        List<String> al = new ArrayList<>();
+        Iterable<Node> nodeIterable = path.nodes();
+
+        //change format of data from: Nodes ->Two Lists
+
+        for (Node node: nodeIterable
+        ) {
+            Map nodeMap = node.asMap();
+            String actorID ;
+            if (node.hasLabel("Actor")){
+                actorID = nodeMap.get("actorID").toString();
+                al.add(actorID);
+            }
+        }
+
+
+        return al;
+    }
+
 }
